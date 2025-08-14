7193e31e8370f39c891c862f0e3915f3f004d9d0
parasaill <lasvegas2992@gmail.com>
2025-08-14 21:29:03 +0200

    Initialization of Project

---
+++ src/main/java/dumplings/SpringBootRestaurantAppApplication.java
@@ -0,0 +1,13 @@
+package dumplings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestaurantAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestaurantAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dumplings/SpringBootRestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dumplings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestaurantAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
