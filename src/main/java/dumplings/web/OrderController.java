4cd3af59330e3c50d765ea860d2dfb380771d069
parasaill <lasvegas2992@gmail.com>
2025-08-15 11:43:17 +0200

    Creation of OrderController

---
+++ src/main/java/dumplings/web/OrderController.java
@@ -0,0 +1,23 @@
+package dumplings.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import lombok.extern.slf4j.Slf4j;
+import dumplings.DumplingOrder;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("dumplingOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+}
