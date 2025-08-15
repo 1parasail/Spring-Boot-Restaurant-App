94a0bc8c6041194ad6b8d41c38b0dc3dd36b6195
parasaill <lasvegas2992@gmail.com>
2025-08-15 12:13:56 +0200

    Connection to JPA

--- src/main/java/dumplings/Dumpling.java
+++ src/main/java/dumplings/Dumpling.java
@@ -1,5 +1,6 @@
 package dumplings;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -7,12 +8,18 @@ import lombok.Data;
 import java.util.List;
 
 @Data
+@Entity
 public class Dumpling {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
 
+    @ManyToMany
     @NotNull
     @Size(min=1, message="You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
