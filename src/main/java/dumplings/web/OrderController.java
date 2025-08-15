0544b845a442b70509a5913884fabfd2a0ca4794
parasaill <lasvegas2992@gmail.com>
2025-08-15 11:57:41 +0200

    Creation of ProcessOrderMethod

--- src/main/java/dumplings/web/OrderController.java
+++ src/main/java/dumplings/web/OrderController.java
@@ -2,6 +2,7 @@ package dumplings.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -18,6 +19,14 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(DumplingOrder order,
+                               SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 
 }
