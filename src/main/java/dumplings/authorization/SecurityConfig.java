e6fee3dba67c54339e9236e519b797277609c8ce
parasaill <lasvegas2992@gmail.com>
2025-09-27 08:45:46 +0200

    Initialization of Authorization Server and basic Security Config

---
+++ src/main/java/dumplings/authorization/SecurityConfig.java
@@ -0,0 +1,36 @@
+package dumplings.authorization;
+
+import dumplings.data.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecuirFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form.loginPage("/login").permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> userRepository.findByUsername(username);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
